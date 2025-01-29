4f3ecbf053c8e793b07c249fd73af0b4034ce3d4
p.andhika <p.andhika@op.iitg.ac.in>
2025-01-29 15:34:07 +0700

    refactor: streamline code formatting in HelperConfig, PriceConverter, and MockV3Aggregator

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -26,9 +26,7 @@ contract HelperConfig is Script {
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory sepoliaConfig = NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        });
+        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
 
         return sepoliaConfig;
     }
@@ -40,15 +38,10 @@ contract HelperConfig is Script {
 
         // Deploy the mocks then return the mock address
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
-            DECIMAL,
-            INITIAL_PRICE
-        );
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMAL, INITIAL_PRICE);
         vm.stopBroadcast();
 
-        NetworkConfig memory anvilConfig = NetworkConfig({
-            priceFeed: address(mockPriceFeed)
-        });
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
 
         return anvilConfig;
     }
