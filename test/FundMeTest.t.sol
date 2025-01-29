6eb511da2e80ac433e0b58d50f1705951af9b11d
p.andhika <p.andhika@op.iitg.ac.in>
2025-01-29 14:40:18 +0700

    feat: update FundMe contract to accept price feed address and modify conversion logic

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -11,11 +11,16 @@ contract FundMeTest is Test {
         fundMe = new FundMe();
     }
 
-    function testMinimumDollarIsFive() public {
+    function testMinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
 
-    function testOwnerIsMsgSender() public {
+    function testOwnerIsMsgSender() public view {
         assertEq(fundMe.i_owner(), address(this));
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
